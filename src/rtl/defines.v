b358d3e98c45761ff36dd961907cf9126f750962
UCASHurui <ucasfred@gmail.com>
2021-09-17 16:20:01 +0800

    add defines.v

---
+++ src/rtl/defines.v
@@ -0,0 +1,6 @@
+`define PC_SIZE 32                 //  PC width
+`define XLEN 32                     //  regfile reg width  
+`define RFIDX_WIDTH 5        //   regfile addr width
+`define INSTR_SIZE 32          //    instruction size
+
+ 
