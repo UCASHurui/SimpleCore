d603131bfb5b9938af661b99aa1ed8637fed1223
UCASHurui <ucasfred@gmail.com>
2021-09-29 13:47:54 +0800

    add itcm & dtcm

---
+++ src/rtl/itcm_ram.v
@@ -0,0 +1,30 @@
+//=================================================
+//Description: itcm ram
+//Author : Hurui
+//Modules: itcm_ram
+//=================================================
+`include "defines.v"
+
+module itcm_ram (
+    input clk,
+    input we,
+    input [`ITCM_RAM_AW-1:0] addr,
+    input [`ITCM_RAM_DW-1:0] din,
+    input [`ITCM_RAM_MW-1:0] wem,
+    output [`ITCM_RAM_DW-1:0] dout,
+);
+    sim_ram#(
+    .DP(`ITCM_RAM_DP),
+    .DW(`ITCM_RAM_DW),
+    .MW(`ITCM_RAM_MW),
+    .AW(`ITCM_RAM_AW),
+    .FORCE_X2ZERO(1)
+    ) u_itcm_gnrl_ram(
+    .clk(clk),
+    .din(din),
+    .addr(addr),
+    .we(we),
+    .wem(wem)
+    .dout(dout)
+);
+endmodule
---
+++ src/rtl/srams.v
@@ -0,0 +1,41 @@
+//=================================================
+//Description: srams containing ITCM and DTCM
+//Author : Hurui
+//Modules: srams
+//=================================================
+`include "defines.v"
+
+module srams (
+    input clk,
+    //ITCM ram
+    input itcm_ram_we,
+    input [`ITCM_RAM_AW-1:0] itcm_ram_addr,
+    input [`ITCM_RAM_DW-1:0] itcm_ram_din,
+    input [`ITCM_RAM_MW-1:0] itcm_ram_wem,
+    output [`ITCM_RAM_DW-1:0] itcm_ram_dout,
+    //DTCM ram
+    input dtcm_ram_we,
+    input [`ITCM_RAM_AW-1:0] dtcm_ram_addr,
+    input [`ITCM_RAM_DW-1:0] dtcm_ram_din,
+    input [`ITCM_RAM_MW-1:0] dtcm_ram_wem,
+    output [`ITCM_RAM_DW-1:0] dtcm_ram_dout,
+);
+
+itcm_ram u_itcm_ram (
+        .clk(clk),
+        .we(itcm_ram_we),
+        .addr(itcm_ram_addr),
+        .din(itcm_ram_din),
+        .wem(itcm_ram_wem),
+        .dout(itcm_ram_dout)
+        );
+
+dtcm_ram u_dtcm_ram (
+        .clk(clk),
+        .we(dtcm_ram_we),
+        .addr(dtcm_ram_addr),
+        .din(dtcm_ram_din),
+        .wem(dtcm_ram_wem),
+        .dout(dtcm_ram_dout)
+        );
+endmodule
