5904117fb2692e4f3b354faf376b98b3548244d1
Fu-Zhou-2019 <2716559421@qq.com>
2021-10-12 21:43:56 +0800

    req

--- src/general/sim_ram.v
+++ src/general/sim_ram.v
@@ -19,9 +19,8 @@ module sim_ram#(
     input[AW-1:0] addr,
     input we,
     input[MW-1:0] wem,//write enable mask
-    output[DW-1:0] dout
+    output[DW-1:0] dout,
 );
-
     reg [DW-1:0] mem_r[0:DP-1];
     reg [AW-1:0] addr_r;
     wire [MW-1:0] wen;
@@ -36,7 +35,6 @@ module sim_ram#(
             addr_r <= addr;
         end
     end
-<<<<<<< HEAD
     
     wire[DW-1:0] dout_pre;
     assign dout_pre = mem_r[addr];
@@ -49,57 +47,6 @@ module sim_ram#(
             mem_r[1] = 32'b0000000_00000_00000_001_00001_011_0110;//lui
             mem_r[2] = 32'b0000000_00000_00000_001_00001_011_0101;//lui
             mem_r[3] = 32'b0000000_00000_00000_001_00001_011_0100;//lui
-=======
-    
-    genvar i;
-    generate
-        for(i = 0; i<MW; i = i + 1) begin
-            if((8*i+8) > DW) begin
-                always @(posedge clk) begin
-                    if (wen[i]) begin
-                        mem_r[addr][DW-1:8*i] <= din[DW-1:8*i];
-                    end
-                end
-            end
-            else begin
-                always @(posedge clk) begin
-                    if (wen[i]) begin
-                        mem_r[addr][8*i+7:8*i] <= din[8*i+7:8*i];
-                    end
-                end
-            end
-        end
-    endgenerate
-    
-    wire [DW-1:0] dout_pre;
-    assign dout_pre = mem_r[addr_r];
-    assign dout = dout_pre;
-    /*
-      generate
-       if(FORCE_X2ZERO == 1) begin: force_x_to_zero
-          for (i = 0; i < DW; i = i+1) begin:force_x_gen 
-              `ifndef SYNTHESIS//{
-             assign dout[i] = (dout_pre[i] === 1'bx) ? 1'b0 : dout_pre[i];
-              `else//}{
-             assign dout[i] = dout_pre[i];
-              `endif//}
-          end
-       end
-       else begin:no_force_x_to_zero
-         assign dout = dout_pre;
-       end
-      endgenerate
-    */
-    
-    
-    initial begin
-        if (ITCM == 1) begin
-            mem_r[0] <= 32'b0000000_00000_00000_001_00001_011_0111;//lui
-            mem_r[1] <= 32'b0000000_00000_00000_001_00001_011_0111;//lui
-            mem_r[2] <= 32'b0000000_00000_00000_001_00001_011_0111;//lui
-            mem_r[3] <= 32'b0000000_00000_00000_001_00001_011_0111;//lui
->>>>>>> 3cc96cc598ee1870011e45f9ad3faf27b1a26f50
         end
     end
-
 endmodule
