8f48968eb904e11a0568adac2b5cf5e28b93850d
UCASHurui <ucasfred@gmail.com>
2021-10-14 17:02:19 +0800

    load wbck fixed

--- src/rtl/dtcm_ctrl.v
+++ src/rtl/dtcm_ctrl.v
@@ -25,14 +25,25 @@ module dtcm_ctrl (
     output [`DTCM_RAM_AW-1:0] dtcm_ram_addr,
     output [`DTCM_RAM_MW-1:0] dtcm_ram_wem,
     output [`DTCM_RAM_DW-1:0] dtcm_ram_din,
-    input [`DTCM_RAM_DW-1:0] dtcm_ram_dout
+    input [`DTCM_RAM_DW-1:0] dtcm_ram_dout,
+
+    input clk,
+    input rst_n
 );
+    wire lsu2dtcm_rsp_valid_r;
     //generate handshake signals with lsu for simulation purpose
     assign lsu2dtcm_cmd_ready = 1;//only lsu can access dtcm, so dtcm is always ready for lsu
-    assign lsu2dtcm_rsp_valid = lsu2dtcm_cmd_valid;//dtcm rsp data come out in the same cycle, so always valid when there is cmd from lsu
+    assign lsu2dtcm_rsp_valid = lsu2dtcm_rsp_valid_r;
     assign lsu2dtcm_rsp_rdata = dtcm_ram_dout;
     assign dtcm_ram_we = ~lsu2dtcm_cmd_read;
     assign dtcm_ram_addr = lsu2dtcm_cmd_addr[`DTCM_ADDR_WIDTH-1:2];
     assign dtcm_ram_wem = lsu2dtcm_cmd_wmask;
     assign dtcm_ram_din = lsu2dtcm_cmd_wdata;
+
+
+    wire lsu2dtcm_rsp_valid_set = lsu2dtcm_cmd_valid;
+    wire lsu2dtcm_rsp_valid_clr = lsu2dtcm_rsp_valid_r;
+    wire lsu2dtcm_rsp_valid_nxt = lsu2dtcm_rsp_valid_set | (~lsu2dtcm_rsp_valid_clr);
+    wire lsu2dtcm_rsp_valid_ena = 1'b1;
+    gnrl_dfflr #(1) lsu2dtcm_rsp_valid_dfflr(lsu2dtcm_rsp_valid_ena, lsu2dtcm_rsp_valid_nxt, lsu2dtcm_rsp_valid_r, clk, rst_n);
 endmodule
