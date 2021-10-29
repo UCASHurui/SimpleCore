fa6d2003086a41dab4484d274d85c3a5eab7497b
UCASHurui <ucasfred@gmail.com>
2021-10-29 15:09:28 +0800

    coremark runnable

--- src/tb/tb_cpu_top.v
+++ src/tb/tb_cpu_top.v
@@ -17,22 +17,19 @@ module tb_cpu_top;
     
     initial begin
         rst_n = 0;
-        pc_rtvec = `PC_SIZE'h0000_0080;
+        pc_rtvec = `PC_SIZE'h0000_0070;
         #30 rst_n = 1;
-        @(posedge clk)
-        #8000 $finish;
+        //@(posedge clk)
+        //#8000 $finish;
     end 
     reg[31:0] instr_in;
     integer i;
     integer test_file;
     initial begin
-        test_file = $fopen("C:\\Users\\hurui\\Desktop\\test_insr\\rv32ui-p-lb","rb");
-        for(i=0; i<5000; i=i+1) begin
+        test_file = $fopen("C:\\Users\\hurui\\Desktop\\test_insr\\a_debug","rb");
+        for(i=0; i<16000; i=i+1) begin
             if($fread(instr_in, test_file)) begin
                 $display(instr_in);
-                if(i==240) begin
-                    $display(instr_in);
-                end
                 u_cpu_top.u_srams.u_itcm_ram.u_itcm_gnrl_ram.mem_r[i] =  {instr_in[7:0],instr_in[15:8],instr_in[23:16],instr_in[31:24]};
                 u_cpu_top.u_srams.u_dtcm_ram.u_dtcm_gnrl_ram.mem_r[i] =  {instr_in[7:0],instr_in[15:8],instr_in[23:16],instr_in[31:24]};
             end
